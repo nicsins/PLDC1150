d162b263c8b57241ef258fbb5375658d7f2c3c76
nic <DominicB@email.com>
2018-10-03 01:09:10 -0500

    updated avg_grade_fun_plus  w numpy and input library !can input different numbers of tests for each student

--- avg_test_fun_plus.py
+++ avg_test_fun_plus.py
@@ -3,18 +3,27 @@
 import numpy as np
 from library import inUtils as inu
 
+
 def get_Input():
     return np.array([inu.putPosFloat(f'please enter test {i+1} ')for i in range(inu.posPutInt("how may test would you like to enter?"))])
+def process_loop(students):
+    for student in range (students):
+        print (f'for student #{student+1}')
+        nums = get_Input()
+        print(f'for student #{student+1}')
+        print_Output(nums)
 
 
 def print_Output(nums):
-    print(f" Total of  all test is {len(nums)} is {np.sum(nums)} \n"
+    print(f" Total of  all {len(nums)} tests, is {np.sum(nums)} \n"
           f"The highest score is{np.max(nums)} \n"
           f"The lowest score is {np.min (nums)}\n"
-          f"The average score was {np.mean(nums)}\n"
+          f"The average score was {np.mean(nums):.2f}\n"
           f"The total of the middle three scores is {np.sum(nums)-(np.min(nums)+np.max(nums))}")
 
 if __name__ == '__main__':
-    nums = get_Input()
-    print_Output(nums)
+    students=inu.posPutInt('How many students grades would you like to record')
+    process_loop(students)
+
+
 
